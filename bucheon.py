8645f919347ecdcedc3c744131d3547adaf15fb0
dia2024 <156995376+dia2024@users.noreply.github.com>
2024-11-25 12:26:27 +0900

    Add files via upload

--- bucheon.py
+++ bucheon.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from streamlit_folium import folium_static
 import pandas as pd
 from folium.plugins import HeatMap
 import folium
 bc = pd.read_csv('bucheon.csv')
+st.title('부천시 편의점 위치')
+st.dataframe(bc)
+
 centerx = bc['위도'].mean()
 centery = bc['경도'].mean()
 m = folium.Map(location=[centerx,centery], zoom_start=15)
 HeatMap(data=bc[['위도','경도']], radius=20).add_to(m)
-st.write(m)
+folium_static(m)
