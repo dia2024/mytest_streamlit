1e5cbaf81a696879f7e49f6ad2f0d3cfcb4c37e1
dia2024 <156995376+dia2024@users.noreply.github.com>
2024-11-25 12:15:19 +0900

    Add files via upload

---
+++ bucheon.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from folium.plugins import HeatMap
+import folium
+bc = pd.read_csv('bucheon.csv')
+centerx = bc['위도'].mean()
+centery = bc['경도'].mean()
+m = folium.Map(location=[centerx,centery], zoom_start=15)
+HeatMap(data=bc[['위도','경도']], radius=20).add_to(m)
+m
