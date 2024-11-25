0d68a39c491776ddd9c2e3139687ff520c359a1a
dia2024 <156995376+dia2024@users.noreply.github.com>
2024-11-25 12:37:23 +0900

    Add files via upload

--- bucheon.py
+++ bucheon.py
@@ -7,8 +7,17 @@ bc = pd.read_csv('bucheon.csv')
 st.title('부천시 편의점 위치')
 st.dataframe(bc)
 
-# centerx = bc['위도'].mean()
-# centery = bc['경도'].mean()
-# m = folium.Map(location=[centerx,centery], zoom_start=15)
-# HeatMap(data=bc[['위도','경도']], radius=20).add_to(m)
-# folium_static(m)
+import folium
+from streamlit_folium import st_folium
+centerx = bc['위도'].mean()
+centery = bc['경도'].mean()
+# center on Liberty Bell, add marker
+m = folium.Map(location=[centerx, centery], zoom_start=16)
+for name, lat, long, c in zip(bc['상호명'], bc['위도'], bc['경도'], bc['color']):
+    folium.Marker([lat, long], popup=name,
+              icon=folium.Icon(color=c)).add_to(m)
+
+# call to render Folium map in Streamlit
+# st_data = st_folium(m, width=725)
+
+st_folium(m, width=725)
